9f4854b0a75d0d7a5d73fe7bd70263cfe3a4e1ed
ikhwalkh <203380321+ikhwalkh@users.noreply.github.com>
2026-04-27 15:44:12 +0700

    first commit

---
+++ Tugas.cpp
@@ -0,0 +1,8 @@
+// buatlah sebuah program yang berisi 2 class dimana terdapat class layang - layang, Belag ketupat.
+// disetiap class nya berisikan luas dan keliling serta input dan output
+// setiap variable memiliki akses private, Bangundatar Belah ketupat merupakan friend function keliling dari layar
+// semua class hanya dapat di akses menggunakan object
+
+
+#include <iostream>
+#include <
